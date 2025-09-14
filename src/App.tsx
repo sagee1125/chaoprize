a6ad031285287c8640e56847cdb5059a194e0584
sa1jing <sa1jing.chen@polyu.edu.hk>
2025-09-15 02:07:12 +0800

    fix: layout

--- src/App.tsx
+++ src/App.tsx
@@ -25,32 +25,40 @@ const ContactUs = () => <div>CONTACT US</div>;
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        {/* INTRODUCTION */}
-        <Route path="/" element={<Home />} />
-        <Route path="/abouttheprize" element={<AboutThePrize />} />
-        <Route path="/theorganiser" element={<TheOrganiser />} />
-        <Route path="/thepatron" element={<ThePatron />} />
+      <div className="flex flex-col min-h-screen">
+        {/* Header */}
+        <Header />
 
-        {/* AWARDS & LAUREATES */}
-        <Route path="/awards" element={<Awards />} />
-        <Route path="/laureates" element={<Laureates />} />
+        {/* 內容區域 */}
+        <main className="flex-1">
+          <Routes>
+            {/* INTRODUCTION */}
+            <Route path="/" element={<Home />} />
+            <Route path="/abouttheprize" element={<AboutThePrize />} />
+            <Route path="/theorganiser" element={<TheOrganiser />} />
+            <Route path="/thepatron" element={<ThePatron />} />
 
-        {/* NOMINATION */}
-        <Route path="/nomination" element={<Nomination />} />
-        <Route path="/importantdates" element={<ImportantDates />} />
+            {/* AWARDS & LAUREATES */}
+            <Route path="/awards" element={<Awards />} />
+            <Route path="/laureates" element={<Laureates />} />
 
-        {/* MEDIA */}
-        <Route path="/news" element={<News />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/publications" element={<Publications />} />
+            {/* NOMINATION */}
+            <Route path="/nomination" element={<Nomination />} />
+            <Route path="/importantdates" element={<ImportantDates />} />
 
-        {/* CONTACT US */}
-        <Route path="/contactus" element={<ContactUs />} />
-      </Routes>
+            {/* MEDIA */}
+            <Route path="/news" element={<News />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/publications" element={<Publications />} />
 
-      <Footer />
+            {/* CONTACT US */}
+            <Route path="/contactus" element={<ContactUs />} />
+          </Routes>
+        </main>
+
+        {/* Footer */}
+        <Footer />
+      </div>
     </Router>
   );
 }
